0bee79d6101a93f23238f7a4022d4de7171143fe
pdhung3012 <pdhung3012@gmail.com>
2022-06-12 00:24:49 -0500

    insert

---
+++ Classifier/EvaluateMLModels.py
@@ -0,0 +1,114 @@
+import os
+
+from lazypredict.Supervised import LazyClassifier
+from UtilFunctions import createDirIfNotExist
+import pandas as pd
+import fasttext
+import numpy as np
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+strEL=' _EL_ '
+
+def getVectorArr(model,strInput):
+    strInput=strInput.replace('\n',strEL)
+    vectorItem = model.get_sentence_vector(strInput)
+    # strVector = ' '.join(map(str, vectorItem))
+    return vectorItem
+def convertListTextToListVector(model,lstText):
+    lstVector=[]
+    for item in lstText:
+        vectorItem=getVectorArr(model,item)
+        lstVector.append(vectorItem)
+    return lstVector
+
+fopResultPragFormer='/home/hungphd/git/Open_OMP/repResult/'
+fopResultAllMLApproaches='/home/hungphd/git/Open_OMP/ensembleMLApproaches/'
+createDirIfNotExist(fopResultPragFormer)
+createDirIfNotExist(fopResultAllMLApproaches)
+lstConfigs=['directive','private','reduction']
+strBert='bert'
+
+for config in lstConfigs:
+    dictAllPredictions = {}
+    dictOracles={}
+    fopStatisticsForProblems=fopResultAllMLApproaches+config+'/'
+    createDirIfNotExist(fopStatisticsForProblems)
+    fpItemOverallStatistics = fopStatisticsForProblems + 'overallAccuracy.csv'
+    fpItemOverallPrediction = fopStatisticsForProblems + 'overallPredictions.csv'
+    dictAllPredictions[strBert]={}
+    for foldIndex in range(1,11):
+        nameSubFolder='{}/fold-{}/'.format(config,foldIndex)
+        fpItemCSV=fopResultPragFormer+nameSubFolder+'data.csv'
+        fpItemModelStatistics=fopResultPragFormer+nameSubFolder+'ensemble_models.csv'
+        fpItemModelPredictions = fopResultPragFormer + nameSubFolder + 'ensemble_predictions.csv'
+        fpItemBertPrediction = fopResultPragFormer + nameSubFolder + 'saved_weights_full_results.txt'
+        pdEnsembleMLs = pd.read_csv(fpItemModelPredictions)
+
+        if foldIndex==1:
+            pdData=pd.read_csv(fpItemCSV)
+            for i in range(0,len(pdData)):
+                strKey=pdData['id'][i]
+                val=pdData['label'][i]
+                dictOracles[strKey]=val
+            for key in dictOracles.keys():
+                dictAllPredictions[strBert][key]=-1
+
+            for i in range(0, len(pdEnsembleMLs.columns)):
+                colName = pdEnsembleMLs.columns[i]
+                if colName == 'realId' or colName == 'Unnamed: 0':
+                    continue
+                dictAllPredictions[colName] = {}
+                for key in dictOracles.keys():
+                    dictAllPredictions[colName][key] = -1
+
+        lstKeysEnsemble=pdEnsembleMLs['realId'].tolist()
+        for i in range(0,len(pdEnsembleMLs.columns)):
+            colName=pdEnsembleMLs.columns[i]
+            if colName=='realId' or colName=='Unnamed: 0':
+                continue
+            lstValueColumn=pdEnsembleMLs[colName].tolist()
+            for j in range(0,len(lstKeysEnsemble)):
+                dictAllPredictions[colName][lstKeysEnsemble[j]]=lstValueColumn[j]
+                # if lstKeysEnsemble[j] == 'jhuber6_ompBLAS_parallel_for_simd_misc_messages.c_85':
+                #     print('fold {}'.format(foldIndex))
+                #     input('abc')
+
+        f1=open(fpItemBertPrediction,'r')
+        arrBertPrediction=f1.read().strip().split('\n')
+        f1.close()
+        for i in range(0,len(arrBertPrediction)):
+            arrTabItems=arrBertPrediction[i].split('\t')
+            # if len(arrTabItems)>=3:
+            strKey=lstKeysEnsemble[i]
+            # if strKey=='jhuber6_ompBLAS_parallel_for_simd_misc_messages.c_85':
+            #     print('fold {}'.format(foldIndex))
+            #     input('abc')
+            val=(int) (arrTabItems[1])
+            dictAllPredictions[strBert][strKey]=val
+        print('end {}'.format(nameSubFolder))
+    dfAllResults=pd.DataFrame()
+    dfAllResults=dfAllResults.assign(realId=list(dictOracles.keys()))
+    dfAllResults=dfAllResults.assign(label=list(dictOracles.values()))
+
+    lstOracles = list(dictOracles.values())
+    lstStrAccuracy=['"Model",Accuracy,Precison,Recall,F1']
+    for keyModel in dictAllPredictions.keys():
+        dictKeyModel=dictAllPredictions[keyModel]
+        lstValueColumn=[]
+        index=0
+        for key2 in dictOracles.keys():
+            # print('{} {} {}'.format(len(dictKeyModel.keys()),keyModel, key2))
+            lstValueColumn.append(dictKeyModel[key2])
+        strMetric='"{}",{},{},{},{}'.format(keyModel,accuracy_score(lstOracles,lstValueColumn),accuracy_score(lstOracles,lstValueColumn),precision_score(lstOracles,lstValueColumn),recall_score(lstOracles,lstValueColumn),f1_score(lstOracles,lstValueColumn))
+        lstStrAccuracy.append(strMetric)
+        dfAllResults[keyModel]=lstValueColumn
+    dfAllResults.to_csv(fpItemOverallPrediction,index=False)
+    f1=open(fpItemOverallStatistics,'w')
+    f1.write('\n'.join(lstStrAccuracy))
+    f1.close()
+
+
+
+
+
+
+
---
+++ Classifier/EvaluateMLModels_step2_statistics.py
@@ -0,0 +1,142 @@
+import os
+
+from lazypredict.Supervised import LazyClassifier
+from UtilFunctions import createDirIfNotExist
+import pandas as pd
+import fasttext
+import numpy as np
+
+strEL=' _EL_ '
+
+def getVectorArr(model,strInput):
+    strInput=strInput.replace('\n',strEL)
+    vectorItem = model.get_sentence_vector(strInput)
+    # strVector = ' '.join(map(str, vectorItem))
+    return vectorItem
+def convertListTextToListVector(model,lstText):
+    lstVector=[]
+    for item in lstText:
+        vectorItem=getVectorArr(model,item)
+        lstVector.append(vectorItem)
+    return lstVector
+
+def getListOfForLoopAndPragma(lstRealIds,lstLabels,fopDatabase,maxLength):
+    lstCodes=[]
+    lstPragmas=[]
+    for i in range(0,len(lstRealIds)):
+        fpItemCode=fopDatabase+lstRealIds[i]+'/code.c'
+        f1=open(fpItemCode,'r')
+        strItemCode=f1.read().strip()
+        f1.close()
+        if len(strItemCode)>maxLength:
+            lenSubStr=maxLength
+        else:
+            lenSubStr=len(strItemCode)
+        strSubStr=strItemCode[:lenSubStr]
+        lstCodes.append(strSubStr)
+        strPragma=''
+        if lstLabels[i]==1:
+            f1=open(fopDatabase+lstRealIds[i]+'/pragma.c','r')
+            strPragma=f1.read().strip()
+            f1.close()
+        lstPragmas.append(strPragma)
+    return lstCodes,lstPragmas
+
+
+
+fopResultPragFormer='/home/hungphd/git/Open_OMP/repResult/'
+fopDatabasePragFormer='/home/hungphd/git/Open_OMP/database/'
+fopResultAllMLApproaches='/home/hungphd/git/Open_OMP/ensembleMLApproaches/'
+createDirIfNotExist(fopResultPragFormer)
+createDirIfNotExist(fopResultAllMLApproaches)
+lstConfigs=['directive','private','reduction']
+strBert='bert'
+
+for config in lstConfigs:
+    dictAllPredictions = {}
+    dictOracles={}
+    fopStatisticsForProblems=fopResultAllMLApproaches+config+'/'
+    createDirIfNotExist(fopStatisticsForProblems)
+    fpItemOverallStatistics = fopStatisticsForProblems + 'overallAccuracy.csv'
+    fpItemOverallPrediction = fopStatisticsForProblems + 'overallPredictions.csv'
+    fopItemDetails=fopStatisticsForProblems+'details/'
+    createDirIfNotExist(fopItemDetails)
+
+    dfAllPredictions=pd.read_csv(fpItemOverallPrediction)
+    lstModelNames=[]
+    for colName in dfAllPredictions.columns:
+        # print(colName)
+        if colName=='realId' or colName=='label':
+            continue
+        lstModelNames.append(colName)
+    # input('aaaa ')
+    lstRealIds=dfAllPredictions['realId'].tolist()
+    lstLabels=dfAllPredictions['label'].tolist()
+    lstCodes,lstPragmas=getListOfForLoopAndPragma(lstRealIds,lstLabels,fopDatabasePragFormer,500)
+    lstCountCorrectModels=[]
+    for i in range(0,len(lstRealIds)):
+        expectValue=lstLabels[i]
+        countCorrectClassifiers=0
+        for modelName in lstModelNames:
+            predictValue=dfAllPredictions[modelName][i]
+            # print('{} {} {}'.format(lstRealIds[i],expectValue,predictValue))
+            if expectValue==predictValue:
+                countCorrectClassifiers+=1
+        lstCountCorrectModels.append(countCorrectClassifiers)
+    dfCountCorrect=pd.DataFrame()
+    dfCountCorrect['realId'] = lstRealIds
+    dfCountCorrect['code']=lstCodes
+    dfCountCorrect['pragma'] = lstPragmas
+    dfCountCorrect['numOfCorrectPredicts']=lstCountCorrectModels
+    dfCountCorrect['label'] = lstLabels
+    dfCountCorrect=dfCountCorrect.sort_values(by=['numOfCorrectPredicts'])
+    dfCountCorrect.to_csv(fopResultAllMLApproaches+config+'/statisticAllLabels_all.csv',index=False)
+    dfPositiveLabels=dfCountCorrect[dfCountCorrect['label']==1]
+    dfNegativeLabels = dfCountCorrect[dfCountCorrect['label'] == 0]
+    dfPositiveLabels.to_csv(fopResultAllMLApproaches+config+'/statisticAllLabels_positive.csv',index=False)
+    dfNegativeLabels.to_csv(fopResultAllMLApproaches + config + '/statisticAllLabels_negative.csv', index=False)
+
+    for modelName in lstModelNames:
+        fopItemModel=fopItemDetails+'/'+modelName+'/'
+        createDirIfNotExist(fopItemModel)
+        lstPredictValueOfModel=dfAllPredictions[modelName].tolist()
+        dfEachModel=pd.DataFrame()
+        dfEachModel['realId'] = lstRealIds
+        dfEachModel['predict'] = lstPredictValueOfModel
+        dfEachModel['label'] = lstLabels
+        dfEachModel['numOfCorrectPredicts'] = lstCountCorrectModels
+        dfEachModel['code'] = lstCodes
+        dfEachModel['pragma'] = lstPragmas
+        dfEachModel=dfEachModel.sort_values(by=['numOfCorrectPredicts'])
+        dfEachModel.to_csv(fopItemModel+'all.csv',index=False)
+        dfTP=dfEachModel[(dfEachModel['predict']==1) & (dfEachModel['label']==1)].sort_values(by=['numOfCorrectPredicts'])
+        dfTN = dfEachModel[(dfEachModel['predict'] == 0) & (dfEachModel['label'] == 0)].sort_values(by=['numOfCorrectPredicts'])
+        dfFP=dfEachModel[(dfEachModel['predict']==1) & (dfEachModel['label']==0)].sort_values(by=['numOfCorrectPredicts'],ascending=False)
+        dfFN = dfEachModel[(dfEachModel['predict'] == 0) & (dfEachModel['label'] == 1)].sort_values(by=['numOfCorrectPredicts'],ascending=False)
+        dfTP.to_csv(fopItemModel + 'TP.csv', index=False)
+        dfTN.to_csv(fopItemModel + 'TN.csv', index=False)
+        dfFP.to_csv(fopItemModel + 'FP.csv', index=False)
+        dfFN.to_csv(fopItemModel + 'FN.csv', index=False)
+        createDirIfNotExist(fopResultAllMLApproaches+'excel/'+config+'/')
+        writer = pd.ExcelWriter(fopResultAllMLApproaches+'excel/'+config+'/'+modelName+'.xlsx', engine='xlsxwriter')
+        dfTP.to_excel(writer, sheet_name='True Positive')
+        dfFP.to_excel(writer, sheet_name='False Positive')
+        dfTN.to_excel(writer, sheet_name='True Negative')
+        dfFN.to_excel(writer, sheet_name='False Negative')
+        dfEachModel.to_excel(writer, sheet_name='all')
+        writer.save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
